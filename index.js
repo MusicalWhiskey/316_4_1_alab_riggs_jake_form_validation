6f29fdc3ad48541f5a61b52f200ed6ca91711ecc
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-10-01 09:19:43 -0700

    test

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 console.log("316.4.1 Form Validation ALAB");
 
-const errorDisplay = document.getElementById("errorDisplay");
+// function showError(message) {
+//     const errorDisplay = document.getElementById('errorDisplay');
+//     errorDisplay.style.display = 'block';
+//     errorDisplay.innerHTML = message;
+// }
 
+// function hideError() {
+//     const errorDisplay = document.getElementById('errorDisplay');
+//     errorDisplay.style.display = 'none';
+//     errorDisplay.innerHTML = '';
+// }
